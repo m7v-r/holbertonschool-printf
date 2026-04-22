9634a833be4c4bfaeb1295da5b38e9a727a48c4a
m7v-r <ms4571690@icloud.com>
2026-04-22 14:07:16 +0000

    Task 4: add prototypes for u, o, x, X

--- main.h
+++ main.h
@@ -25,7 +25,7 @@ int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i, va_list list, char buffer[],
 	int flags, int width, int precision, int size);
 
-/* Prototypes for functions */
+/* الدوال الأساسية */
 int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_string(va_list types, char buffer[],
@@ -36,6 +36,17 @@ int print_int(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_binary(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
-int print_binary(va_list types, char buffer[],
-    int flags, int width, int precision, int size);
+
+/* دوال التاسك 4 (u, o, x, X) */
+int print_unsigned(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_octal(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_hexadecimal(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_hexa_upper(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_hexa(va_list types, char map_to[], char buffer[],
+	int flags, char flag_ch, int width, int precision, int size);
+
 #endif
